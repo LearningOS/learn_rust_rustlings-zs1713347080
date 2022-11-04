add237f7a9614e103ed33fd47f0e44b8fb0ca78a
zs1713347080 <wwww.1713347080@qq.com>
2022-11-04 23:35:38 +0800

    error5

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,15 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
+// 
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    let mut current = 4;
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
+        *i = current;
+        current += 4;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
